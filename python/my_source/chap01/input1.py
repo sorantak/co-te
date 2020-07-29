c77710d5c2dc5ac1ab5b3d5b4a60c952be48fd14
Hansol Tak <takyhsoul@gmail.com>
2020-07-30 02:43:13 +0900

    01-1: Add input1.py, max3_func.py

---
+++ python/my_source/chap01/input1.py
@@ -0,0 +1,9 @@
+print('Input your name.: ', end='')
+name = input()
+
+# name = input('Input your name.: ')
+
+print(f'Hi, {name}!')
+
+# input()은 python의 함수. 무조건 문자열만 반환. 숫자를 반환하려면 앞에 int(input()) 이런 식으로 처리해줘야 함.
+# print 내에서 f는 문자열 내에서 {} 안의 값을 받도록 함.
